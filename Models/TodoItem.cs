7f14b37ef6e7c257cdb156a2721e68f8b8eb1ba8
cihanerman <cihan.erman@outlook.com>
2018-04-04 23:28:15 +0300

    Tutorial tamamlandı.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace webApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
